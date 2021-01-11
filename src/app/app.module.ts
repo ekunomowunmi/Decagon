531a84d0b29679f8993273ff40d963d7e1ccc068
Omowunmi <omowunmi.ekun@xputertechnologies.com>
2021-01-11 16:04:35 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import {IconsModule, DropdownModule, ButtonsModule, WavesModule, CollapseModule} from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllUsersComponent } from './all-users/all-users.component';
+import { MaleUsersComponent } from './male-users/male-users.component';
+import { FemaleUsersComponent } from './female-users/female-users.component';
+import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllUsersComponent,
+    MaleUsersComponent,
+    FemaleUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IconsModule,
+    HttpClientModule,
+    FormsModule,
+    ButtonsModule.forRoot(),
+    WavesModule.forRoot(),
+    CollapseModule.forRoot(),
+    DropdownModule.forRoot()
+
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
