f2485b7332800af61ab30a2ad961e1788cf82a09
Omowunmi <omowunmi.ekun@xputertechnologies.com>
2021-01-13 12:26:54 +0100

    add routing to user details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AllUsersComponent } from './all-users/all-users.component';
 import { FemaleUsersComponent } from './female-users/female-users.component';
 import { MaleUsersComponent } from './male-users/male-users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 
 const routes: Routes = [
-  {path:'',component: AllUsersComponent},
+  {path:'',component: AllUsersComponent, pathMatch: 'full'},
   {path:'all-users',component:AllUsersComponent},
   {path:'male-users',component:MaleUsersComponent},
-  {path:'female-users',component:FemaleUsersComponent}
+  {path:'female-users',component:FemaleUsersComponent},
+  {path:'user-details',component:UserDetailComponent}
 ];
 
 @NgModule({
