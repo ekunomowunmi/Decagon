3694beda7b6e8bbe0d163584b6747eb5dc171fc9
Omowunmi <omowunmi.ekun@xputertechnologies.com>
2021-01-14 12:32:28 +0100

    remove unused routerlink

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed, async,inject, fakeAsync, tick} from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import {BsDropdownConfig, DropdownModule, IconsModule} from 'angular-bootstrap-md'
 import { FormsModule } from '@angular/forms';
